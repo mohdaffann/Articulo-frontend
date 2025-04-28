14fad49d92a976077a47904866d46d39f75fb93b
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-04-28 22:34:50 +0530

    create publish button for blog

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -6,6 +6,9 @@ function Write() {
         <div className="min-h-screen w-full flex justify-center px-2 sm:px-4">
             <div className="w-full max-w-3xl p-4 sm:p-6">
                 <Editor />
+                <div className="flex justify-end mt-1">
+                    <button className="rounded-md px-2 py-1 transition-all text-white duration-300 ease-in-out bg-green-600 hover:bg-green-700 ">Publish</button>
+                </div>
             </div>
 
 
