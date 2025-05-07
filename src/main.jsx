660ca64676c4e1859cf4e23f0e48e4321927f628
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-05-07 23:38:07 +0530

    init react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-
 import { createRoot } from 'react-dom/client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import App from './App'
-import authStore from './store/authStore'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+
   </>
 
 
