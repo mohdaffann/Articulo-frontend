9bfe6f3386b133fe4fb2ee72062d37ac1eaa7a7b
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-05-07 23:39:15 +0530

    shift Posts fetching via react-query

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,37 +1,50 @@
 import React from "react";
 import PostCard from "../components/PostCard";
 import axios from "axios";
-import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'
+import { useQuery } from "@tanstack/react-query";
+import EditorLoader from "../components/EditorLoader";
 function Posts() {
-    const [post, setPost] = useState([])
 
-    const fetchAll = async () => {
-        try {
-            const res = await axios.get('/v1/blog');
-            console.log(res.data);
-
-            setPost(res.data.blogs)
-            console.log();
-
-        } catch (error) {
-            console.log('error in getting all blogs', error);
-
+    const response = useQuery(
+        {
+            queryKey: ['posts'],
+            queryFn: async () => {
+                return await axios.get('v1/blog')
+            },
+            staleTime: 6 * 60 * 1000
         }
+    )
+
+    if (response.isLoading) {
+        return (
+
+            <div className="flex flex-row lg:flex-row max-w-5xl mx-auto px-5 py-5 my-10">
+
+                <div className="w-full lg:w-2/3 space-y-6">
+                    {
+                        [...Array(7)].map((_, ind) => (
+                            <EditorLoader key={ind} />
+                        ))
+                    }
+                </div>
+
+            </div>
+
+        )
+    }
+    if (response.isError) {
+        return <div>{Error}</div>
     }
 
-    useEffect(() => {
-        fetchAll();
-    },
-        []
-    );
+
     return (
         <div className="flex flex-row lg:flex-row max-w-5xl mx-auto px-5 py-5 my-10">
             {/* leftt posts display*/}
             <div className="w-full lg:w-2/3 space-y-6">
-                {post.map((post, index) => (
-                    <Link to={`/posts/${post._id}`} >
-                        <PostCard key={index} post={post} />
+                {response?.data?.data?.blogs.map((post, index) => (
+                    <Link to={`/posts/${post._id}`} key={post._id} >
+                        <PostCard post={post} />
                     </Link>
 
                 ))}
