4999ee446b52ea3bdff7f3726ad1efa326bec1da
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-05-09 23:49:45 +0530

    Transparent Header and other UI changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
 
   return (
 
-    <header className="w-full border-b border-gray-600 bg-white">
+    <header className="w-full  bg-white/60 backdrop-blur-sm  fixed top-0">
       <div className="container mx-auto px-4 py-2 flex items-center justify-between">
         {/* Logo */}
         <Link to="/" className="flex items-center">
