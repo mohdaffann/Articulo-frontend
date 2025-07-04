9c3ea79d7dc64e136363abd3aa0cadb9f074ef3d
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-07-04 21:51:18 +0530

    fix code block

--- src/components/CodeRenderer.jsx
+++ src/components/CodeRenderer.jsx
@@ -10,10 +10,10 @@ export const CodeRenderer = ({ data }) => {
         setTimeout(() => setCopy(false), 2000);
     }
     return (
-        <div className="flex max-w-full mt-6 px-4 py-6 flex-col  bg-gray-100    rounded-lg ">
+        <div className="flex w-full   mt-6 px-4 py-6 flex-col  bg-gray-100    rounded-lg ">
             <div className="flex  flex-row border-b-2 mb-1.5 border-b-gray-300 justify-between items-center px-4 py-2 bg-gray-100">
                 <span className="text-pink-500  text-[18px] font-mono">
-                    {data.language + ' lang' || 'code'}
+                    {data.language || 'Code'}
                 </span>
                 {
                     data.showCopyButton && (
@@ -31,17 +31,17 @@ export const CodeRenderer = ({ data }) => {
                     )
                 }
             </div>
-            <pre>
+            <pre className="max-w-full overflow-x-auto md:whitespace-pre whitespace-pre-wrap">
                 {
                     lines.map((item, ind) => (
                         <div key={ind} className="flex ">
                             {
                                 data.showlinenumbers && (
-                                    <span className="text-gray-500 mr-4 select-none text-[19px] text-right inline-block">{ind + 1}</span>
+                                    <span className="text-gray-500 mr-4 select-none text-[19px] text-right inline-block in-w-[2ch] flex-shrink-0">{ind + 1}</span>
 
                                 )
                             }
-                            <code className="flex-1 text-[15px] mb-1.5">
+                            <code className="flex-1 text-[15px] mb-1.5 md:break-normal break-words">
                                 {item}
                             </code>
                         </div>
