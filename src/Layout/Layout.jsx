c09b8399eb748c81fcaec151037c5f6058257014
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-07-04 21:57:32 +0530

    layout refractoring

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-import { Outlet } from "react-router-dom"
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom"
 import Header from "../components/Header";
-
+import authStore from "../store/authStore";
+import FloatingWrite from "../components/FloatingWrite";
+import Footer from "../components/Footer";
+import EditorLoader from "../components/EditorLoader";
 function Layout() {
+    const checkAuth = authStore((s) => s.checkAuth)
+    const isAuthLoading = authStore((s) => s.isAuthLoading)
+    const loc = useLocation();
+    const floatButton = loc.pathname === '/write'
+    const hideHeader = loc.pathname === '/'
+    useEffect(() => {
+        checkAuth()
+    }, [])
+    if (isAuthLoading) {
+        return <EditorLoader />
+    }
     return (
         <>
-            <Header />
-            <Outlet />
+            <div className="flex flex-col min-h-screen">
+                {!hideHeader && <Header />}
+                <main className="flex-grow">
+                    <Outlet />
+                </main>
+                {!floatButton && <FloatingWrite />}
+                <Footer />
+
+            </div>
+
+
+
         </>
     )
 }
