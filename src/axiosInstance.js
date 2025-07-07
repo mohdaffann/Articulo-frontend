422cc4a19d1d2f7efdcfce5f0010411f25773a67
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-07-07 23:38:55 +0530

    code refractoring

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.PROD ? 'https://articulo-backend.onrender.com/api/v1' : 'http://localhost:4000/api/v1';
+const baseURL = import.meta.env.PROD ? 'https://articulo-backend.onrender.com/api' : 'http://localhost:4000/api/v1';
 
 const axiosInstance = axios.create({
     baseURL,
