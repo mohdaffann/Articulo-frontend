89c907fe35254ff81d20408cdaccaa2b961c966c
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-07-08 20:49:42 +0530

    code refractor

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -77,7 +77,7 @@ const Signup = () => {
 
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-sm p-6 bg-white rounded-xl shadow-lg">
-                <h2 className="text-2xl font-bold text-center">Join Blogman</h2>
+                <h2 className="text-2xl font-bold text-center">Join Articulo</h2>
 
 
                 <form className="space-y-4 " onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
